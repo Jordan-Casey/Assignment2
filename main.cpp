157fd6a2478b06e24acee061182144579261e50f
ToastyWheat <jorca1506@gmail.com>
2025-02-02 16:28:34 -0600

    Added code to main.cpp and main.h for Hello World

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-/** 
-Programming Fundamentals II (84770)
-Alexander Katrompas
-Jordan Casey
-2/2/2025
+/*
+* Programming Fundamentals II (84770)
+* Alexander Katrompas
+* Jordan Casey
+* 2/2/2025
+*
+*
+* File: main.h
+* Author: Jordan Casey
 */
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
--- main.h
+++ main.h
@@ -1,6 +1,19 @@
-/** 
-Programming Fundamentals II (84770)
-Alexander Katrompas
-Jordan Casey
-2/2/2025
+
+/*
+* Programming Fundamentals II (84770)
+* Alexander Katrompas
+* Jordan Casey
+* 2/2/2025
+*
+*
+* File: main.h
+* Author: Jordan Casey
 */
+
+
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
